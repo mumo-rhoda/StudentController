10b015a09e283cebca362b60aaa46b4741eb2052
mumo-rhoda <rhodamumo117@gmail.com>
2022-09-06 15:30:31 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Component
+public class StudentService {
+
+    public List<Student> getStudents(){
+
+        return List.of(new Student(
+                1L,
+                "Mirriam",
+                "mirriam@gmail.com",
+                LocalDate.of(2000, Month.FEBRUARY,5),
+                21
+
+        ));
+
+    }
+}
